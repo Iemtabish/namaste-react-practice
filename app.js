8fd669a7c0970b8b96610cdb2d16c138c3a2e511
iemtabish <tabishabbasmzp@gmail.com>
2025-08-26 00:28:52 +0530

    ep1

---
+++ app.js
@@ -0,0 +1,34 @@
+  const parent = React.createElement(
+    "div",
+    { id: "parent" },
+
+    React.createElement(
+      "div",
+      { id: "child" },
+
+      [
+        React.createElement("h1", {}, "I'm the Header"),
+        React.createElement("h2", {}, "I'm the Header2"),
+      ]
+    ),  React.createElement(
+      "div",
+      { id: "child" },
+
+      [
+        React.createElement("h1", {}, "I'm the Header"),
+        React.createElement("h2", {}, "I'm the Header2"),
+      ]
+    )      
+  );
+
+
+
+            const heading = React.createElement(
+                
+                "h1",
+                {id: "heading" , xyz: "abc"}, 
+                "Hello World from JS!")
+
+            const root = ReactDOM.createRoot(document.getElementById("root"))
+
+            root.render(parent)
